97f2b7f44cd4330333c33c00fa40444c8205e0ca
henryyou <henryyou@fb.com>
2014-10-18 21:25:53 -0700

    Initial Commit

---
+++ RottenTomatoesApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoesApp
+//
+//  Created by Jin You on 10/18/14.
+//  Copyright (c) 2014 Henryyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
